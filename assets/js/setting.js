845bdab94c84e7e57c54eab6bc388880ef8f9b81
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-05-11 08:04:52 -0500

    a

--- assets/js/setting.js
+++ assets/js/setting.js
@@ -7,7 +7,6 @@ form.addEventListener('submit', (event) => {
   const fotoPerfil = document.querySelector('#foto-perfil').files[0];
   const fotoBanner = document.querySelector('#foto-banner').files[0];
 
-  // Validar entradas de usuario
   if (nombre.trim() === '') {
     alert('Por favor, introduce un nombre válido.');
     return;
@@ -21,28 +20,22 @@ form.addEventListener('submit', (event) => {
     return;
   }
 
-  // Almacenar datos en una base de datos
-  // Este código depende del tipo de base de datos que estés utilizando
 
-  // Actualizar la interfaz de usuario con los cambios realizados
   actualizarInterfazUsuario(nombre, biografia, fotoPerfil, fotoBanner);
 });
 
 function actualizarInterfazUsuario(nombre, biografia, fotoPerfil, fotoBanner) {
-  // Actualizar el nombre
+
   const nombreElemento = document.querySelector('.nombre');
   nombreElemento.textContent = nombre;
 
-  // Actualizar la biografía
   const biografiaElemento = document.querySelector('.biografia');
   biografiaElemento.textContent = biografia;
 
-  // Actualizar la foto de perfil
   const fotoPerfilElemento = document.querySelector('.foto-perfil');
   const fotoPerfilURL = URL.createObjectURL(fotoPerfil);
   fotoPerfilElemento.setAttribute('src', fotoPerfilURL);
 
-  // Actualizar la foto de banner
   const fotoBannerElemento = document.querySelector('.foto-banner');
   const fotoBannerURL = URL.createObjectURL(fotoBanner);
   fotoBannerElemento.setAttribute('src', fotoBannerURL);
