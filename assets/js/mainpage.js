33b8d6725d547c16de27d583c1e0b3f416e4946a
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-05-12 07:38:40 -0500

    PORFIN

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -1,32 +1,42 @@
-fetch('https://raw.githubusercontent.com/Lina17Landys/Twisted-wonderland/master/assets/twstData.json')
-  .then(response => response.json())
-  .then(data => {
-    const eventosDiv = document.getElementById('eventos');
-    data.events.forEach(evento => {
-      const img = document.createElement('img');
-      img.src = evento.img;
-      eventosDiv.querySelector('.lista-eventos').appendChild(img);
-    });
-
-    const personajesDiv = document.getElementById('personajes');
-    data.characters.forEach(personaje => {
-      const icon = document.createElement('img');
-      icon.src = personaje.icon;
-      personajesDiv.querySelector('.lista-personajes').appendChild(icon);
-      icon.addEventListener('click', () => {
-        const personajeInfo = document.getElementById('personajeInfo');
-        const personajeNombre = document.getElementById('personajeNombre');
-        const personajeImagen = document.getElementById('personajeImagen');
-
-        personajeNombre.textContent = personaje.fullName;
-        personajeImagen.src = personaje.NonGroovyPic;
-
+  // Función para cargar los datos JSON utilizando fetch
+  const loadJSON = () => {
+    return fetch('https://raw.githubusercontent.com/Lina17Landys/Twisted-wonderland/master/assets/twstData.json')
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Error loading JSON:', error);
+        throw error;
       });
-    });
+  };
 
-    const characters = data.characters;
-    window.twstCharacters = characters; 
-  })
-  .catch(error => {
-    console.error('Error al obtener los datos:', error);
-  });
+  // Cargar los datos del archivo JSON
+  loadJSON('https://raw.githubusercontent.com/Lina17Landys/Twisted-wonderland/master/assets/twstData.json')
+    .then(data => {
+      // Obtener los eventos del objeto JSON
+      const events = data.events;
+      const eventsContainer = document.getElementById('eventsContainer');
+
+      // Recorrer los eventos y mostrar las imágenes
+      events.forEach(event => {
+        const img = document.createElement('img');
+        img.src = event.img;
+        eventsContainer.appendChild(img);
+      });
+
+      // Obtener los personajes del objeto JSON
+      const characters = data.characters;
+      const charactersContainer = document.getElementById('charactersContainer');
+
+      // Recorrer los personajes y mostrar los iconos
+      characters.forEach((character, index) => {
+        const icon = document.createElement('img');
+        icon.src = character.icon;
+        icon.addEventListener('click', () => {
+          // Redireccionar al hacer clic en el icono con el índice del personaje seleccionado
+          window.location.href = `object.html?index=${index}`;
+        });
+        charactersContainer.appendChild(icon);
+      });
+    })
+    .catch(error => {
+      console.error('Error loading JSON:', error);
+    });
--- assets/js/object.js
+++ assets/js/object.js
@@ -1,1 +1,54 @@
-//pendiente
+// Función para obtener parámetros de la URL
+const getParameterByName = (name, url) => {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  };
+
+  // Obtener el índice del personaje seleccionado de los parámetros de la URL
+  const selectedIndex = parseInt(getParameterByName('index'));
+
+  // Cargar los datos del archivo JSON utilizando fetch
+  fetch('https://raw.githubusercontent.com/Lina17Landys/Twisted-wonderland/master/assets/twstData.json')
+    .then(response => response.json())
+    .then(data => {
+      // Obtener los personajes del objeto JSON
+      const characters = data.characters;
+
+      // Verificar si el índice seleccionado es válido
+      if (selectedIndex >= 0 && selectedIndex < characters.length) {
+        const selectedCharacter = characters[selectedIndex];
+
+        // Mostrar los detalles del personaje seleccionado
+        const characterDetails = document.getElementById('characterDetails');
+
+        const img = document.createElement('img');
+        img.src = selectedCharacter.NonGroovyPic;
+        characterDetails.appendChild(img);
+
+        const fullName = document.createElement('p');
+        fullName.textContent = "Full Name: " + selectedCharacter.fullName;
+        characterDetails.appendChild(fullName);
+
+        const maxPow = document.createElement('p');
+        maxPow.textContent = "Max Power: " + selectedCharacter.maxPow;
+        characterDetails.appendChild(maxPow);
+
+        const maxHp = document.createElement('p');
+        maxHp.textContent = "Max HP: " + selectedCharacter.maxHp;
+        characterDetails.appendChild(maxHp);
+
+        const spell1 = document.createElement('p');
+        spell1.textContent = "Spell 1: " + selectedCharacter.spell1;
+        characterDetails.appendChild(spell1);
+
+        const spell2 = document.createElement('p');
+        spell2.textContent = "Spell 2: " + selectedCharacter.spell2;
+        characterDetails.appendChild(spell2);
+      } 
+    })
+
