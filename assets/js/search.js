b3302907d6e20ddaa8034416e93eb6977be7ce06
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-05-17 18:19:30 -0500

    barra

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,14 +1,14 @@
-function searchItem() {
-    let input = document.getElementById('searchBar').value
-    input=input.toLowerCase();
+const searchItem = () => {
+    let input = document.getElementById('searchBar').value.toLowerCase();
     let x = document.getElementsByClassName('events', 'characters');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
+
+    for (let i = 0; i < x.length; i++) {
+        if (x[i].classList.contains('events')) {
+            x[i].style.display = "charactersContainer";
+        } else if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "characters";
+        } else {
+            x[i].style.display = "eventsContainer";
         }
     }
 }
