48e97750499d039253fbbee42252a499490d4876
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-06-03 10:06:50 -0500

    otros arreglos

--- assets/js/saved.js
+++ assets/js/saved.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const saveContainer = document.getElementById('saveContainer');
-  
     const savedIcons = localStorage.getItem('savedIcons') ? JSON.parse(localStorage.getItem('savedIcons')) : [];
-  
-    savedIcons.forEach(iconSrc => {
+    
+    savedIcons.forEach((iconSrc, index) => {
       const icon = document.createElement('img');
       icon.src = iconSrc;
       icon.classList.add('icon');
       saveContainer.appendChild(icon);
+      icon.addEventListener('click', () => {
+        window.location.href = `object.html?index=${index}`;
+      });
     });
   });
   
